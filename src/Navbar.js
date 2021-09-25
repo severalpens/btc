05f10336761f5a0e4cd3f03a5ce6c95d210c0ed9
severalpens <severalpens@protonmail.com>
2021-09-25 20:11:31 +1000

    master

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+
+function Navbar() {
+  return (
+      <div className="Navbar">
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Send</a>
+          <a href="/">Receive</a>
+          
+      </div>
+  );
+}
+
+export default Navbar;
