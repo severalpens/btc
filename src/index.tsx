853d290ad3d1e329798e9460f22de3ee04008623
Severalpens <Paul.Collins@ibm.com>
2022-02-11 07:20:30 +1100

    master

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Invoices from './routesArchive/invoices';
-import Invoice from './routesArchive/invoice';
 import TransferEth from './routes/transferEth'
-import Accounts from './routesArchive/accounts';
 import Contracts from './routes/contracts';
-import RegisterContract from './routesArchive/registerContract';
 import Seed from './routesArchive/seed';
-import Approve from './routesArchive/approve';
+import Approve from './routes/approve';
 import ExitTransaction from './routesArchive/exitTransaction';
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
@@ -19,49 +15,35 @@ Amplify.configure(awsExports);
 
 const rootElement = document.getElementById('root');
 
-if(typeof window.ethereum !== 'undefined'){
-ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-      <Route path="invoices" element={<Invoices />}>
-        <Route
-          index
-          element={
-            <main style={{ padding: '1rem' }}>
-              <p>Select an invoice</p>
-            </main>
-          }
-        />
-        <Route path=":invoiceId" element={<Invoice />} />
-      </Route>
+if (typeof window.ethereum !== 'undefined') {
+  ReactDOM.render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="transferEth" element={<TransferEth />} />
+          <Route path="contracts" element={<Contracts />} />
+          <Route path="seed" element={<Seed />} />
+          <Route path="approve" element={<Approve />} />
+          <Route path="exitTransaction" element={<ExitTransaction />} />
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>,
+    rootElement
+  );
+}
+else {
+  ReactDOM.render(
+    <NoMetamask />,
+    document.getElementById('root')
+  );
 
-      <Route path="transferEth" element={<TransferEth />}/>
-      <Route path="accounts" element={<Accounts />}/>
-      <Route path="contracts" element={<Contracts />}/>
-      <Route path="registerContract" element={<RegisterContract />}/>
-      <Route path="seed" element={<Seed />}/>
-      <Route path="approve" element={<Approve />}/>
-      <Route path="exitTransaction" element={<ExitTransaction />}/>
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: '1rem' }}>
-              <p>There's nothing here!</p>
-            </main>
-          }
-        />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
-  rootElement
-);
-        }
-        else{
-          ReactDOM.render(
-              <NoMetamask />,
-            document.getElementById('root')
-          );
-        
-        }
+}
 
--- src/routes/contracts.jsx
+++ src/routes/contracts.jsx
@@ -1,119 +1,110 @@
-/* src/App.js */
-import { useEffect, useState } from 'react';
-import { API, graphqlOperation } from 'aws-amplify';
-import { createContract } from '../graphql/mutations';
+//TODO: Refactor this component. Split into smaller components.
+import React, { useState, useEffect } from "react";
+import parseFile from "./parseFile";
+import { API } from 'aws-amplify';
 import { listContracts } from '../graphql/queries';
+import { createContract} from '../graphql/mutations';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
 
-import { ethers } from "ethers";
 
+//Material UI Specifications:
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  table: {
+    minWidth: 650,
+  },
+}));
 
-const initialState = { symbol: '', name: '', address: '' }
+const initialState = { symbol: '', name: '', initialAmount: '', artifact: {}, created: 0 }
 
-const Contracts = () => {
-  const [formState, setFormState] = useState(initialState)
-  const [contracts, setContracts] = useState([])
+//Uploader component is the main functional component.
+const Uploader = () => {
 
+  // material UI specification
+  const classes = useStyles(); 
+
+  //React hooks used to read/write variables
+  const [artifact, setArtifact] = useState([]);
+  const [contract, setContract] = useState([]);
+  const [contracts, setContracts] = useState([]);
+  let fileReader;
+
+  //TODO: Auto-populate contracts list on load. 
   useEffect(() => {
-    fetchContracts()
-  }, [])
+  }, []);
 
+  
+  const handleFileRead = (e) => {
+    const content = fileReader.result;
+  };
 
-
-  function setInput(key, value) {
-    setFormState({ ...formState, [key]: value })
-  }
-
-
-
-  async function fetchContracts() {
-    try {
-      const contractData = await API.graphql(graphqlOperation(listContracts))
-      const contracts = contractData.data.listContracts.items
-      setContracts(contracts)
-    } catch (err) { console.log('error fetching contracts') }
-  }
-
-
-
-  async function addContract() {
-    try {
-      if (!formState.name || !formState.address) return
-      const contract = { ...formState }
-      setContracts([...contracts, contract])
-      setFormState(initialState)
-      await API.graphql(graphqlOperation(createContract, {input: contract}))
-    } catch (err) {
-      console.log('error creating contracts:', err)
-    }
-  }
-
-  async function newContract(){
-
-      const requestURL = '../artifacts/BurnToClaim.json';
-      const request = new Request(requestURL);
-      const response = await fetch(request);
-      const artifact = await response.json();
-    
-    
-    
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const factory = new ethers.ContractFactory(artifact.abi, artifact.bytecode, signer);
-    const deployment = await factory.deploy(window.ethereum.selectedAddress);
-    const result = await deployment.deployed();
-
-    // contract.address = result.address
-  }
+  //Refer to https://dev.to/ilonacodes/frontend-shorts-how-to-read-content-from-the-file-input-in-react-1kfb
+  const handleFileChosen = (file) => {
+    fileReader = new FileReader();
+    fileReader.onloadend = handleFileRead;
+    fileReader.readAsText(file);
+  };
 
   return (
-    <div style={styles.container}>
-
-      <h2>Contracts</h2>
-      <input
-        onChange={event => setInput('symbol', event.target.value)}
-        style={styles.input}
-        value={formState.symbol}
-        placeholder="Symbol"
-      />
-      <input
-        onChange={event => setInput('name', event.target.value)}
-        style={styles.input}
-        value={formState.name}
-        placeholder="Name"
-      />
-      <input
-        onChange={event => setInput('initialAmount', event.target.value)}
-        style={styles.input}
-        value={formState.initialAmount}
-        placeholder="Initial Amount (if applicable)"
-      />
-      <input
-        onChange={event => setInput('artifact', event.target.value)}
-        style={styles.input}
-        value={formState.artifact}
-        placeholder="Json Artifact  File (abi & bytecode)"
-      />
-
-      <button style={styles.button} onClick={newContract}>Submit</button>
-      {
-        contracts.map((contract, index) => (
-          <div key={contract.id ? contract.id : index} style={styles.todo}>
-            <p style={styles.contractName}>{contract.name}</p>
-            <p style={styles.contractAddress}>{contract.address}</p>
-          </div>
-        ))
-      }
+    <div className="uploader" >
+      <h3>Load Contracts</h3>
+      <div className="input-group file-chooser">
+        <div className="custom-file">
+          <input type="file" className="custom-file-input" onChange={e => handleFileChosen(e.target.files[0])} />
+          <label className="custom-file-label" htmlFor="inputGroupFile04">Choose file</label>
+        </div>
+      </div>
+      <div>
+        <div className="obd">
+          <h3>Contracts</h3>
+        </div>
+        <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+          <TableCell>Network</TableCell>
+          <TableCell>Symbol</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Address</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {contracts.map((contract) => (
+            <TableRow key={contract.id}>
+              <TableCell  component="th" scope="row">
+                {contract.sku}
+              </TableCell>
+              <TableCell>{contract.network}</TableCell>
+              <TableCell>{contract.symbol}</TableCell>
+              <TableCell>{contract.name}</TableCell>
+              <TableCell>{contract.address}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+      </div>
     </div>
   )
 }
 
-const styles = {
-  container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  contractName: { fontSize: 20, fontWeight: 'bold' },
-  contractAddress: { marginBottom: 0 },
-  button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
-}
+export default Uploader;
 
-export default Contracts;
