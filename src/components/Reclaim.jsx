dc146acc61e0e7e9970fbc9abbce5d223d8420a9
severalpens <severalpens@protonmail.com>
2022-03-20 19:40:40 +1100

    add blank method components

---
+++ src/components/Reclaim.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Reclaim() {
+  return (
+    <div>Reclaim</div>
+  )
+}
---
+++ src/components/RegisterContract.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RegisterContract() {
+  return (
+    <div>RegisterContract</div>
+  )
+}
