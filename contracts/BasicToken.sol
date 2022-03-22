7fa1887ce13d5522515d0785e5acf06bba79edaa
Severalpens <Paul.Collins@ibm.com>
2022-03-22 17:58:49 +1100

    re-add hardhat

--- contracts/BasicToken.sol
+++ contracts/BasicToken.sol
@@ -2,10 +2,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
 // Example class - a mock class using delivering from ERC20
 contract BasicToken is ERC20 {
-  constructor(uint256 initialBalance) ERC20("Basic", "BSC")  {
+  constructor(uint256 initialBalance) ERC20("Basic", "BSC") {
     _mint(msg.sender, initialBalance);
   }
 }
