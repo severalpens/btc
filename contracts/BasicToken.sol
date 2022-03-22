445662403129551a7562ce11447e2a08efd366c0
Severalpens <Paul.Collins@ibm.com>
2022-03-22 14:54:48 +1100

    remove truffle

--- contracts/BasicToken.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract MyCollectible is ERC20 {
-    constructor(name, symbol) ERC20(name, symbol) {
-
-    }
-}
