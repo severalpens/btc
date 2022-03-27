ecbfc0f287eab2642657d69db15f53843d7ff641
severalpens <severalpens@protonmail.com>
2022-03-28 06:33:51 +1100

    Did forms up to update. Add and entryTransaction to go.

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -8,73 +8,36 @@ const blockchain = new Blockchain();
 
 export default function Add(props) {
 
-  const [tokenAddress, setTokenAddress] = useState('0x0');
-  const [tokenTransferAmount, setTokenTransferAmount] = useState('1000');
-  const [contracts, setContracts] = useState(null);
-
-
+  const [contractAddress, setContractAddress] = useState(null);
+  const [transactionId, setTransactionId] = useState(null);
+  const [hashSecret, setHashSecret] = useState(null);
   const [ctError1, setCtError1] = useState('');
   const [ibError1, setIbError1] = useState('');
 
   useEffect(() => {
 
     async function fetchData() {
-      let graphqlResult = await API.graphql({ query: queries.listContracts });
-      let contract = JSON.parse(window.localStorage.getItem('contract'));
-      let cl = graphqlResult.data.listContracts.items.filter(x => !x._deleted && contract.address !== x.address);
-      setContracts(cl);
-      setTokenAddress(cl[0].address)
     }
     
     fetchData();
    }, []);
  
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     let contract = JSON.parse(window.localStorage.getItem('contract'));
-    await blockchain.registerContract(contract, tokenAddress,tokenTransferAmount);
-  }
-
-  const handleContractChange = async (e) => {
-    setTokenAddress(e.target.value);
+    // await blockchain.exitTransaction(contract, tokenAddress,tokenTransferAmount);
   }
 
 
   return (
     <div className="ml-16 my-16 ">
-      <h2 className="font-medium leading-tight text-4xl mt-0 mb-8 text-blue-600">Add</h2>
+      <h2 className="font-medium leading-tight text-4xl mt-0 mb-8 text-blue-600">Reclaim Transaction</h2>
       <form className="" onSubmit={handleSubmit}>
-        <div className="mb-3 xl:w-96">
-          <label className="form-label inline-block mb-2 text-gray-700" htmlFor="contract-type">Token (ERC20 Contract) Address</label>
-          <select id='contract-type' name='contract-type' className="
-            form-select 
-            appearance-none
-            block
-            w-800
-            px-3
-            py-1.5
-            text-base
-            font-normal
-            text-gray-700
-            bg-white bg-clip-padding bg-no-repeat
-            border border-solid border-gray-300
-            rounded
-            transition
-            ease-in-out
-            m-0
-            focus:text-gray-700 
-            focus:bg-white 
-            focus:border-blue-600 
-            focus:outline-none"
-            aria-label="Default select example" onChange={handleContractChange}>
-          {contracts? contracts.map((contract) => (
-            <option key={contract.id} value={contract.address}>{contract.address}</option>
-          )):''}
-          </select>
-        </div>
+        {/* Contract Address */}
         <div className="mb-3 xl:w-96" >
-          <label htmlFor="exampleFormControlInput1" className="form-label inline-block mb-2 text-gray-700">
-            Initial Transfer Amount
+          <label htmlFor="contractAddress" className="form-label inline-block mb-2 text-gray-700">
+            Contract Address
           </label >
           <input type="text" className="
       form-control
@@ -96,13 +59,83 @@ export default function Add(props) {
       focus:border-blue-600 
       focus:outline-none
     "
-            placeholder="10000"
-            id="initialBalance"
-            name="initialBalance"
-            defaultValue="1000"
-            onChange={e => setTokenTransferAmount(e.target.value)}
+            placeholder="0x0"
+            id="contractAddress"
+            name="contractAddress"
+            defaultValue="0x0"
+            onChange={e => setContractAddress(e.target.value)}
           />
         </div>
+
+        {/* Transaction Id */}
+        <div className="mb-3 xl:w-96" >
+          <label htmlFor="transactionId" className="form-label inline-block mb-2 text-gray-700">
+            Transaction Id 
+          </label >
+          <input type="text" className="
+      form-control
+      block
+      w-full
+      px-3
+      py-1.5
+      text-base
+      font-normal
+      text-gray-700
+      bg-white bg-clip-padding
+      border border-solid border-gray-300
+      rounded
+      transition
+      ease-in-out
+      m-0
+      focus:text-gray-700
+      focus:bg-white 
+      focus:border-blue-600 
+      focus:outline-none
+    "
+            placeholder="0x0"
+            id="transactionId"
+            name="transactionId"
+            defaultValue="0x0"
+            onChange={e => setTransactionId(e.target.value)}
+          />
+        </div>
+
+
+
+        {/* Hash Secret */}
+        <div className="mb-3 xl:w-96" >
+          <label htmlFor="hashSecret" className="form-label inline-block mb-2 text-gray-700">
+            Hash Secrect 
+          </label >
+          <input type="text" className="
+      form-control
+      block
+      w-full
+      px-3
+      py-1.5
+      text-base
+      font-normal
+      text-gray-700
+      bg-white bg-clip-padding
+      border border-solid border-gray-300
+      rounded
+      transition
+      ease-in-out
+      m-0
+      focus:text-gray-700
+      focus:bg-white 
+      focus:border-blue-600 
+      focus:outline-none
+    "
+            placeholder="0x0"
+            id="hashSecret"
+            name="hashSecret"
+            defaultValue="0x0"
+            onChange={e => setHashSecret(e.target.value)}
+          />
+        </div>
+
+
         <button type="submit" className="border px-6 py-2.5 border-black rounded-md">Submit</button>
       </form>
     </div>
--- src/components/ExitTransaction.jsx
+++ src/components/ExitTransaction.jsx
@@ -8,10 +8,13 @@ const blockchain = new Blockchain();
 
 export default function ExitTransaction(props) {
 
-  const [tokenAddress, setTokenAddress] = useState('0x0');
-  const [tokenTransferAmount, setTokenTransferAmount] = useState('1000');
   const [contracts, setContracts] = useState(null);
-
+  const [burnAddress, setBurnAddress] = useState('0x0');
+  const [hash, setHash] = useState('0x0');
+  const [periodEndSeconds, setPeriodEndSeconds] = useState('0x0');
+  const [tokenAddress, setTokenAddress] = useState('0x0');
+  const [amount, setAmount] = useState('1');
+  
 
   const [ctError1, setCtError1] = useState('');
   const [ibError1, setIbError1] = useState('');
@@ -29,14 +32,27 @@ export default function ExitTransaction(props) {
     fetchData();
    }, []);
  
+
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     let contract = JSON.parse(window.localStorage.getItem('contract'));
-    await blockchain.registerContract(contract, tokenAddress,tokenTransferAmount);
+    // await blockchain.exitTransaction(contract, tokenAddress,tokenTransferAmount);
   }
 
   const handleContractChange = async (e) => {
     setTokenAddress(e.target.value);
+  }
+
+  const setInput = (inputName, value) => {
+    switch (inputName) {
+      case 'burnAddress':
+        setBurnAddress(value)
+        break;
+    
+      default:
+        break;
+    }
   }
 
 
@@ -44,37 +60,10 @@ export default function ExitTransaction(props) {
     <div className="ml-16 my-16 ">
       <h2 className="font-medium leading-tight text-4xl mt-0 mb-8 text-blue-600">Exit Transaction</h2>
       <form className="" onSubmit={handleSubmit}>
-        <div className="mb-3 xl:w-96">
-          <label className="form-label inline-block mb-2 text-gray-700" htmlFor="contract-type">Token (ERC20 Contract) Address</label>
-          <select id='contract-type' name='contract-type' className="
-            form-select 
-            appearance-none
-            block
-            w-800
-            px-3
-            py-1.5
-            text-base
-            font-normal
-            text-gray-700
-            bg-white bg-clip-padding bg-no-repeat
-            border border-solid border-gray-300
-            rounded
-            transition
-            ease-in-out
-            m-0
-            focus:text-gray-700 
-            focus:bg-white 
-            focus:border-blue-600 
-            focus:outline-none"
-            aria-label="Default select example" onChange={handleContractChange}>
-          {contracts? contracts.map((contract) => (
-            <option key={contract.id} value={contract.address}>{contract.address}</option>
-          )):''}
-          </select>
-        </div>
+        {/* Burn Address */}
         <div className="mb-3 xl:w-96" >
           <label htmlFor="exampleFormControlInput1" className="form-label inline-block mb-2 text-gray-700">
-            Initial Transfer Amount
+            Burn Address
           </label >
           <input type="text" className="
       form-control
@@ -96,13 +85,151 @@ export default function ExitTransaction(props) {
       focus:border-blue-600 
       focus:outline-none
     "
-            placeholder="10000"
-            id="initialBalance"
-            name="initialBalance"
-            defaultValue="1000"
-            onChange={e => setTokenTransferAmount(e.target.value)}
+            placeholder="0x0"
+            id="burnAddress"
+            name="burnAddress"
+            defaultValue="0x0"
+            onChange={e => setBurnAddress(e.target.value)}
           />
         </div>
+
+        {/* Hash */}
+        <div className="mb-3 xl:w-96" >
+          <label htmlFor="exampleFormControlInput1" className="form-label inline-block mb-2 text-gray-700">
+            Hash
+          </label >
+          <input type="text" className="
+      form-control
+      block
+      w-full
+      px-3
+      py-1.5
+      text-base
+      font-normal
+      text-gray-700
+      bg-white bg-clip-padding
+      border border-solid border-gray-300
+      rounded
+      transition
+      ease-in-out
+      m-0
+      focus:text-gray-700
+      focus:bg-white 
+      focus:border-blue-600 
+      focus:outline-none
+    "
+            placeholder="0x0"
+            id="hash"
+            name="hash"
+            defaultValue="0x0"
+            onChange={e => setHash(e.target.value)}
+          />
+        </div>
+
+        {/* period End Seconds */}
+        <div className="mb-3 xl:w-96" >
+          <label htmlFor="exampleFormControlInput1" className="form-label inline-block mb-2 text-gray-700">
+            Period End Seconds
+          </label >
+          <input type="text" className="
+      form-control
+      block
+      w-full
+      px-3
+      py-1.5
+      text-base
+      font-normal
+      text-gray-700
+      bg-white bg-clip-padding
+      border border-solid border-gray-300
+      rounded
+      transition
+      ease-in-out
+      m-0
+      focus:text-gray-700
+      focus:bg-white 
+      focus:border-blue-600 
+      focus:outline-none
+    "
+            placeholder="120"
+            id="periodEndSeconds"
+            name="periodEndSeconds"
+            defaultValue="120"
+            onChange={e => setPeriodEndSeconds(e.target.value)}
+          />
+        </div>
+
+
+        {/* tokenAddress */}
+        <div className="mb-3 xl:w-96" >
+          <label htmlFor="exampleFormControlInput1" className="form-label inline-block mb-2 text-gray-700">
+            Token Address
+          </label >
+          <input type="text" className="
+      form-control
+      block
+      w-full
+      px-3
+      py-1.5
+      text-base
+      font-normal
+      text-gray-700
+      bg-white bg-clip-padding
+      border border-solid border-gray-300
+      rounded
+      transition
+      ease-in-out
+      m-0
+      focus:text-gray-700
+      focus:bg-white 
+      focus:border-blue-600 
+      focus:outline-none
+    "
+            placeholder="0x0"
+            id="tokenAddress"
+            name="tokenAddress"
+            defaultValue="0x0"
+            onChange={e => setTokenAddress(e.target.value)}
+          />
+        </div>
+
+
+
+        {/* Amount */}
+        <div className="mb-3 xl:w-96" >
+          <label htmlFor="exampleFormControlInput1" className="form-label inline-block mb-2 text-gray-700">
+            Amount
+          </label >
+          <input type="text" className="
+      form-control
+      block
+      w-full
+      px-3
+      py-1.5
+      text-base
+      font-normal
+      text-gray-700
+      bg-white bg-clip-padding
+      border border-solid border-gray-300
+      rounded
+      transition
+      ease-in-out
+      m-0
+      focus:text-gray-700
+      focus:bg-white 
+      focus:border-blue-600 
+      focus:outline-none
+    "
+            placeholder="0x0"
+            id="amount"
+            name="amount"
+            defaultValue="1"
+            onChange={e => setAmount(e.target.value)}
+          />
+        </div>
+
+
+
         <button type="submit" className="border px-6 py-2.5 border-black rounded-md">Submit</button>
       </form>
     </div>
