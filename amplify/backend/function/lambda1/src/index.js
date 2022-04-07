af809b1bbf222abe01b91227070dbfee675867e2
Severalpens <Paul.Collins@ibm.com>
2022-04-07 17:12:19 +1000

    add hash generation to api

--- amplify/backend/function/lambda1/src/index.js
+++ amplify/backend/function/lambda1/src/index.js
@@ -1,18 +1,34 @@
 
+var cryptoLib = require("crypto");
 
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+
+  const bufToStr = (b) => "0x" + b.toString("hex");
+  const sha256 = (x) => cryptoLib.createHash("sha256").update(x).digest();
+  const random32 = () => cryptoLib.randomBytes(32);
+
+  function newSecretHashPair() {
+    const secret = random32();
+    const hash = sha256(secret);
     return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-     headers: {
-         "Access-Control-Allow-Origin": "*",
-         "Access-Control-Allow-Headers": "*"
-     }, 
-        body: JSON.stringify('Hello from Lambda!'),
+      secret: bufToStr(secret),
+      hash: bufToStr(hash),
     };
+  };
+
+  return {
+    statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*"
+    },
+    // body: JSON.stringify('Hello from Lambda!'),
+    body: JSON.stringify(newSecretHashPair()),
+  };
 };
