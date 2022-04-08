de5833c4493aec4e666cf1b2206b05eae5aceb3c
Severalpens <Paul.Collins@ibm.com>
2022-04-08 15:57:53 +1000

    update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import Navbar from './components/Navbar1';
+import Navbar from './components/Navbar2';
 import './App.scss';
 import React, {useState} from 'react';
 
